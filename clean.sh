902bcc78dcc5d96d89ba8c21f273d00e52b830be
Eric Dey <eric@deys.org>
2025-10-08 19:59:27 -0500

    fix path detection; output cleanup

--- clean.sh
+++ clean.sh
@@ -3,8 +3,8 @@
 # Clean up build artifacts from the Python build and setuptools modules
 #
 ME=$(basename "$0")
-MYDIR=$(cd "$(dirname "$0") && pwd)")
+MYDIR=$(cd "$(dirname "$0")" && pwd)
 
 cd "$MYDIR" || exit 1  # do or do not, there is no try
-rm -r build/ dist/ *.egg-info
+rm -r build/ dist/ *.egg-info  2> /dev/null
 find . -type d -name '__pycache__' -print0 | xargs -0 rm -r
